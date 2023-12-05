2341df205b0d17e022c99d028269156f3390a1c8
le_woudar <rollandkev@yahoo.fr>
2023-12-05 13:08:36 +0100

    feat(config): allowed_hosts is now a list of str instead of a list of HttpUrl

--- djangosearch/config.py
+++ djangosearch/config.py
@@ -1,5 +1,6 @@
 from secrets import token_hex
-from pydantic import Field, HttpUrl
+
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -7,7 +8,7 @@ class Config(BaseSettings):
     model_config = SettingsConfigDict(env_prefix='search_')
     debug: bool = True
     secret_key: str = Field(default_factory=lambda: token_hex(28))
-    allowed_hosts: list[HttpUrl] = Field(default_factory=list)
+    allowed_hosts: list[str] = Field(default_factory=list)
     db_name: str = 'starwars'
     db_user: str = 'postgres'
     db_password: str = 'hum'
